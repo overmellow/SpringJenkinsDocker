b314fd2b20c7d467eb257a30db525f7800f67701
overmellow <morteza_faraji@yahoo.com>
2022-05-25 13:54:35 -0700

    first commit

---
+++ src/test/java/com/wv/mfaraji/spring/jenkinsdocker/SpringJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wv.mfaraji.spring.jenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
