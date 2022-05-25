f7be9ce63409dc04aa11eca45943218bb2953ac0
overmellow <morteza_faraji@yahoo.com>
2022-05-25 14:14:37 -0700

    removed no args annotation

--- src/main/java/com/wv/mfaraji/spring/jenkinsdocker/ThemeParkRide.java
+++ src/main/java/com/wv/mfaraji/spring/jenkinsdocker/ThemeParkRide.java
@@ -1,5 +1,8 @@
 package com.wv.mfaraji.spring.jenkinsdocker;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -12,7 +15,6 @@ import javax.validation.constraints.NotEmpty;
 @Entity
 @Getter
 @ToString
-@NoArgsConstructor
 public class ThemeParkRide {
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
@@ -24,10 +26,58 @@ public class ThemeParkRide {
   private int thrillFactor;
   private int vomitFactor;
   
+  
+  public ThemeParkRide() {}
+  
   public ThemeParkRide(String name, String description, int thrillFactor, int vomitFactor) {
+	super();
     this.name = name;
     this.description = description;
     this.thrillFactor = thrillFactor;
     this.vomitFactor = vomitFactor;
   }
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getThrillFactor() {
+		return thrillFactor;
+	}
+	
+	public void setThrillFactor(int thrillFactor) {
+		this.thrillFactor = thrillFactor;
+	}
+	
+	public int getVomitFactor() {
+		return vomitFactor;
+	}
+	
+	public void setVomitFactor(int vomitFactor) {
+		this.vomitFactor = vomitFactor;
+	}
+  
+  
+  
+  
 }
