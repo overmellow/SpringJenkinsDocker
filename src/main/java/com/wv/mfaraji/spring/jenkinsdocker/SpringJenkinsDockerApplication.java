f4a14d5680bf74f297bb75cb3899ffac7a56d557
overmellow <morteza_faraji@yahoo.com>
2022-06-01 16:28:46 -0700

    remove repo ride

--- src/main/java/com/wv/mfaraji/spring/jenkinsdocker/SpringJenkinsDockerApplication.java
+++ src/main/java/com/wv/mfaraji/spring/jenkinsdocker/SpringJenkinsDockerApplication.java
@@ -18,8 +18,6 @@ public class SpringJenkinsDockerApplication {
             repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
             repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
             repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
-            repository.save(new ThemeParkRide("Water Flame", "Flame Coming out of water.", 4, 5));
-            System.out.println("hwat");
         };
     
 	}
